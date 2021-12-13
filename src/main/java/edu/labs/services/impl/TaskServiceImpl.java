c869d0b206cd4371323eb4ad0b94b6f27eadbb39
AndriiZhmurkevych <gmand@ukr.net>
2021-12-13 02:40:07 +0200

    added Empoyee service

--- src/main/java/edu/labs/services/impl/TaskServiceImpl.java
+++ src/main/java/edu/labs/services/impl/TaskServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
